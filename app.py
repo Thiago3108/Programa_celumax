88b62bc6e33ec229bd16cf5ca7a4b32e0418b874
Thiago3108 <santiago2234508@correo.uis.edu.co>
2023-06-26 23:56:48 -0500

    Se agrego un boceto de la pagina web, falta la parte de la base de datos y que pase a ser funcional, adicional a eso, hace falta realizar la parte del registro o inicio de las cuentas, para que solo tenga acceso exclusivo las personas que paguen o que sean familia xd

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, request, redirect
 
 app=Flask(__name__)
 
@@ -7,13 +7,35 @@ app=Flask(__name__)
 def inicio():
     return render_template("sitio/index.html")
 
-@app.route("/recibos")
+@app.route("/admin/recibos")
 def recibos():
-    return render_template("sitio/lrecibos.html")
+    return render_template("admin/lrecibos.html")
 
 @app.route("/productos")
 def productos():
     return render_template("sitio/nproductos.html")
+
+@app.route("/admin/adm")
+def adm():
+    return render_template("admin/oadmin.html")
+
+@app.route("/admin")
+def admin_index():
+    return render_template("admin/index.html")
+
+@app.route("/admin/login")
+def admin_login():
+    return render_template("admin/login.html")
+
+@app.route("/admin/adm/guardar", methods=["POST"])
+def admin_productos_guardar():
+    _nombre=request.form["txtNombre"]
+    _imagen=request.files["txtImagen"]
+    _numero=request.form["txtNumber"]
+    print(_nombre)
+    print(_imagen)
+    print(_numero)
+    return redirect("/admin/adm")
 
 if __name__ == "__main__":
     app.run(debug=True)
